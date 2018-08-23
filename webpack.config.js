07d6d82acb38b127a6f6ab1dbc9db197d08a0295
James Adam Wasson <jameswasson1@gmail.com>
2018-08-23 13:53:03 -0500

    moving some stuff

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,12 @@
     const webpack = require('webpack');
 
     module.exports = {
-        mode: "development", // "production" | "development" | "none" 
-        entry: "./src/main.js", // string | object | array 
+        mode: "development", // "production" | "development" | "none"
+        entry: path.resolve(__dirname, "./src/picker.js"), // string | object | array 
         output: {
-            // options related to how webpack emits results
-            path: path.resolve(__dirname, "dist"), // string
-            // the target directory for all output files
-            // must be an absolute path (use the Node.js path module)
-            filename: "./picker.js", // string    // the filename template for entry chunks
-            publicPath: "/assets", // string    // the url to the output directory resolved relative to the HTML page
-            library: "MyLibrary", // string,
-            // the name of the exported library
-            libraryTarget: "umd", // universal module definition    // the type of the exported library
+            path: path.resolve(__dirname, "dist"),
+            filename: "./[name].bundle.js",
+            publicPath: 'http://localhost:9000/'
         },
         module: {
             rules: [
@@ -28,7 +22,7 @@
                     }]
                 },
                 {
-                    test: /\.(s*)css$/,
+                    test: /\.(.?)css$/,
                     use: [{
                         loader: "style-loader"
                     }, {
@@ -41,38 +35,33 @@
                         loader: 'sass-loader'
                     }]
                 },
+                {
+                    test: /\.less$/,
+                    use: [{
+                        loader: 'less-loader'
+                    }]
+                }
             ]
         },
         resolve: {
-            // options for resolving module requests
-            // (does not apply to resolving to loaders)
             modules: [
-                "node_modules",
-                path.resolve(__dirname, "app")
+                "node_modules" ,
             ],
             alias: {
-                'vue': 'vue/dist/vue.common.js'
-            }
+                'vue': 'vue/dist/vue.common.js',
+                "app": 'node_modules'
+            },
         },
-        // configuration for hot reloading
         devServer: {
             overlay: true, // immediate feedback on compiler errors -- will show an overlay on the browser
             compress: true,
+            hot: true,
             port: 9000,
-            publicPath: "http://localhost:9000", // This must match the DEV_SERVER_PATH path in WebpackBundleRenderer.cs.
-            headers: { // CORS stuff necessary to allow main application to request files from webpack-dev-server
-                "Access-Control-Allow-Origin": "http://localhost:25016",
+            headers: {
                 "Access-Control-Allow-Credentials": "true",
                 "Access-Control-Allow-Headers": "Content-Type, Authorization, x-id, Content-Length, X-Requested-With",
                 "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
             }
-        },
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: '"development"'
-                }
-            })
-        ],
+        }
     };
 }());
